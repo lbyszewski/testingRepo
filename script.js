0464d73f77700def1b90ede4a88d729e2c510ad2
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2022-09-11 09:31:58 +0200

    Modyfiction

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 function test(){
     var a = parseInt(document.getElementById('first').value);
     var b = parseInt(document.getElementById('last').value);
+    var i;
+    
 
+    for(var c = a;c<=b;c++){
+        
+        if(c % 3 == 0){
+            document.writeln('<table>' +'<th>' +  c+ '</th>' + '</table>');
+        }
+    }
 
-    for(var i = a;i<=b;i++){
+    let people = [
+        {
+            name: "Adrian",
+            last: "Wąsik"
 
-        document.writeln('<table>' +'<th>' +i + '</th>' + '</table>');
+        },
+        {
+            name: "Tomasz",
+            last: "Wąsik",
+            children: [
+                "1",
+                "2",
+                "3"
+
+            ]
+        },
+        {
+            name: "Anna",
+            last: "Wąsik"
+
+        }
+    ]
+
+    for(let person of people){
+        console.log(`Czesc ${person.name} ${person.children[$[children]]}`);
     }
 }
